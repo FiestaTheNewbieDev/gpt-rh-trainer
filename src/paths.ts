414177ac40972562c077d120e7c22b80b191cd25
fossor_c <fossor_c@etna-alternance.net>
2024-03-21 14:54:47 +0100

    paths.ts added

---
+++ src/paths.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+export const INPUT_FOLDER = process.env.INPUT_FOLDER || path.join(process.cwd(), 'input');
+export const JSON_INPUT_FOLDER = process.env.JSON_INPUT_FOLDER || path.join(INPUT_FOLDER, 'json');
+export const PDF_INPUT_FOLDER = process.env.PDF_INPUT_FOLDER || path.join(INPUT_FOLDER, 'pdf');
+
+export const OUTPUT_FOLDER = process.env.OUTPUT_FOLDER || path.join(process.cwd(), 'output');
+
+export const TEMP_FOLDER = process.env.TEMP_FOLDER || path.join(process.cwd(), 'temp');
