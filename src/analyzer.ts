4352a9c9ecd01b3ac0e3a2d4454fbe21b1354e21
vo_o <vo_o@etna-alternance.net>
2024-03-21 15:40:03 +0100

    [ADD] => modify analyze functions

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -1,7 +1,54 @@
-export function analyzeJSON(filePath: string, outputFilePath: string) {
-    
+import fs from "fs";
+import pdf from "pdf-parse";
+import IMessage from "./interfaces/IMessage";
+import { sendPrompt } from "./openAiController";
+
+export async function analyzeJson(filePath: string, outputFilePath: string) {
+  fs.writeFileSync(outputFilePath, "");
+  const dataBuffer = fs.readFileSync(filePath);
+  const data = JSON.parse(dataBuffer.toString());
+  for (const object of data) {
+    const history: IMessage[] = [
+      {
+        role: "system",
+        content: `Based on the text send by user, determine the user's personality and follow these instructions:\n - ALWAYS return your response as JSON object following this model:\n{\nintraverted_extraverted: number; // [0-100]\nintuitive_observant: number; // [0-100]\nthinking_feeling: number; // [0-100]\njudging_prospecting: number; // [0-100]\nassertive_turbulent: number; // [0-100]\n- Each value of the model should be a number between 0 and 100, 50 being the average`,
+      },
+    ];
+
+    const response = await sendPrompt(object.text, history);
+
+    history.push(
+      { role: "user", content: object.text },
+      { role: "assistant", content: response.data.choices[0].message.content }
+    );
+    console.log(history);
+    fs.appendFileSync(
+      outputFilePath,
+      JSON.stringify({ messages: history }, null) + "\n"
+    );
+  }
 }
 
-export function analyzePDF(filePath: string, outputFilePath: string) {
+export function analyzePdf(filePath: string, outputFilePath: string) {
+  const dataBuffer = fs.readFileSync(filePath);
+  pdf(dataBuffer).then(async (data: any) => {
+    console.log("here")
+    const history: IMessage[] = [
+      {
+        role: "system",
+        content: `Based on the text send by user, determine the user's personality and follow these instructions:\n - ALWAYS return your response as JSON object following this model:\n{\nintraverted_extraverted: number; // [0-100]\nintuitive_observant: number; // [0-100]\nthinking_feeling: number; // [0-100]\njudging_prospecting: number; // [0-100]\nassertive_turbulent: number; // [0-100]\n- Each value of the model should be a number between 0 and 100, 50 being the average`,
+      },
+    ];
 
+    const response = await sendPrompt(data.text, history);
+    history.push(
+      { role: "user", content: data.text },
+      { role: "assistant", content: response.data.choices[0].message.content }
+    );
+
+    fs.appendFileSync(
+      outputFilePath,
+      JSON.stringify({ messages: history }, null) + "\n"
+    );
+  });
 }
--- src/app.ts
+++ src/app.ts
@@ -1,97 +1,88 @@
-import fs from 'fs';
-import path from 'path';
-import pdf from 'pdf-parse';
-import args from 'args';
-import IMessage from './interfaces/IMessage';
-import { sendPrompt } from './openAiController';
-import genPersonality from './genPersonality';
-import Loading from './Loading';
+import fs from "fs";
+import path from "path";
+import args from "args";
+import genPersonality from "./genPersonality";
+import Loading from "./Loading";
+import {
+  INPUT_FOLDER,
+  JSON_INPUT_FOLDER,
+  OUTPUT_FOLDER,
+  PDF_INPUT_FOLDER,
+  TEMP_FOLDER,
+} from "./paths";
+import { analyzeJson, analyzePdf } from "./analyzer";
 
 args
-    .option('gen-personalities', 'Gen personalities to analyze', 0)
-    .option('analyze', 'Analyze inputs to gen training files', false);
+  .option("gen-personalities", "Gen personalities to analyze", 0)
+  .option("analyze", "Analyze inputs to gen training files", false);
 
 const flags = args.parse(process.argv);
 
-const inputFolderPath = path.join(process.cwd(), 'input');
-if (!fs.existsSync(inputFolderPath)) {
-    fs.mkdirSync(inputFolderPath);
+const folders = [
+  INPUT_FOLDER,
+  JSON_INPUT_FOLDER,
+  PDF_INPUT_FOLDER,
+  OUTPUT_FOLDER,
+  TEMP_FOLDER,
+];
+
+for (const folder of folders) {
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
+  }
 }
 
-const pdfInputFolderPath = path.join(inputFolderPath, 'pdf');
-if (!fs.existsSync(pdfInputFolderPath)) {
-    fs.mkdirSync(pdfInputFolderPath);
-}
-
-const jsonInputFolderPath = path.join(inputFolderPath, 'json');
-if (!fs.existsSync(jsonInputFolderPath)) {
-    fs.mkdirSync(jsonInputFolderPath);
-}
-
-const outputFolderPath = path.join(process.cwd(), 'output');
-if (!fs.existsSync(outputFolderPath)) {
-    fs.mkdirSync(outputFolderPath);
-}
-
-const tempFolderPath = path.join(process.cwd(), 'temp');
-if (!fs.existsSync(outputFolderPath)) {
-    fs.mkdirSync(outputFolderPath);
-}
-
-const pdfInputFiles = fs.readdirSync(pdfInputFolderPath);
-const jsonInputFiles = fs.readdirSync(jsonInputFolderPath);
+const jsonInputFiles = fs.readdirSync(JSON_INPUT_FOLDER);
+const pdfInputFiles = fs.readdirSync(PDF_INPUT_FOLDER);
 
 async function main() {
-    if (flags.genPersonalities > 0) {
-        const fileName = `${Date.now()}.json`;
-        const personalities: {text: string}[] = [];
-        const loading = new Loading();
-        for (let i = 0; i < flags.genPersonalities; i++) {
-            loading.start(`Generating personality [${i + 1}/${flags.genPersonalities}]`);
-            personalities.push(await genPersonality(i > flags.genPersonalities / 2));
-            fs.writeFileSync(path.join(jsonInputFolderPath, fileName), '[' + personalities.map(person => JSON.stringify(person)).join(',\n') + ']');
-            loading.stop(`Personality generated! [${i + 1}/${flags.genPersonalities}]`);
-        } 
+  if (flags.genPersonalities > 0) {
+    const fileName = `${Date.now()}.json`;
+    const personalities: { text: string }[] = [];
+    const loading = new Loading();
+    for (let i = 0; i < flags.genPersonalities; i++) {
+      loading.start(
+        `Generating personality [${i + 1}/${flags.genPersonalities}]`
+      );
+      personalities.push(await genPersonality(i > flags.genPersonalities / 2));
+      fs.writeFileSync(
+        path.join(JSON_INPUT_FOLDER, fileName),
+        "[" +
+          personalities.map((person) => JSON.stringify(person)).join(",\n") +
+          "]"
+      );
+      loading.stop(
+        `Personality generated! [${i + 1}/${flags.genPersonalities}]`
+      );
     }
-    if (flags.analyze) {
-        const fileName = `${Date.now()}.json`;
-        const loading = new Loading();
-        for (let i = 0; i < jsonInputFiles.length; i++) {
-            loading.start(`Analyzing JSON inputs [${i + 1}/${jsonInputFiles.length}]`);
-            analyzeJson(path.join(jsonInputFolderPath, jsonInputFiles[i]), path.join(outputFolderPath, fileName));
-            loading.stop(`JSON inputs analyzed! [${i + 1}/${jsonInputFiles.length}]`);
-        }
-        loading.stop('JSON inputs analyzed!');
-        loading.start('Analyzing PDF inputs');
-        analyzePdf(pdfInputFolderPath, outputFolderPath);
-        loading.stop('PDF inputs analyzed!');
+  }
+  if (flags.analyze) {
+    const fileName = `${Date.now()}.json`;
+    const loading = new Loading();
+    for (let i = 0; i < jsonInputFiles.length; i++) {
+      loading.start(
+        `Analyzing JSON inputs [${i + 1}/${jsonInputFiles.length}]`
+      );
+      await analyzeJson(
+        path.join(JSON_INPUT_FOLDER, jsonInputFiles[i]),
+        path.join(OUTPUT_FOLDER, fileName)
+      );
+      loading.stop(`JSON inputs analyzed! [${i + 1}/${jsonInputFiles.length}]`);
     }
-    process.exit(0);
-}
+    loading.start("Analyzing PDF inputs");
 
-function analyzeJson(filePath: string, outputFilePath: string) {
-    const dataBuffer = fs.readFileSync(filePath);
-    const data = JSON.parse(dataBuffer.toString());
-    data.forEach(async (text: string) => {
-        const history: IMessage[] = [{ role: 'system', content: `Based on the text send by user, determine the user's personality and follow these instructions:\n - ALWAYS return your response as JSON object following this model:\n{\nintraverted_extraverted: number; // [0-100]\nintuitive_observant: number; // [0-100]\nthinking_feeling: number; // [0-100]\njudging_prospecting: number; // [0-100]\nassertive_turbulent: number; // [0-100]\n- Each value of the model should be a number between 0 and 100, 50 being the average` }];
-
-        const response = await sendPrompt(text, history);
-        history.push({ role: 'user', content: data.text}, { role: 'assistant', content: response.data.choices[0].message.content });
-
-        fs.appendFileSync(outputFilePath, JSON.stringify({messages: history}, null) + '\n');
+    // made by me
+    pdfInputFiles.forEach((file, index) => {
+      analyzePdf(
+        path.join(PDF_INPUT_FOLDER, file),
+        path.join(OUTPUT_FOLDER, fileName)
+      );
+      loading.stop(
+        `PDF inputs analyzed! [${index + 1}/${pdfInputFiles.length}]`
+      );
     });
-}
-
-function analyzePdf(filePath: string, outputFilePath: string) {
-    const dataBuffer = fs.readFileSync(filePath);
-    pdf(dataBuffer).then(async(data: any) => {
-        const history: IMessage[] = [{ role: 'system', content: `Based on the text send by user, determine the user's personality and follow these instructions:\n - ALWAYS return your response as JSON object following this model:\n{\nintraverted_extraverted: number; // [0-100]\nintuitive_observant: number; // [0-100]\nthinking_feeling: number; // [0-100]\njudging_prospecting: number; // [0-100]\nassertive_turbulent: number; // [0-100]\n- Each value of the model should be a number between 0 and 100, 50 being the average` }]
-
-        const response = await sendPrompt(data.text, history);
-        history.push({ role: 'user', content: data.text}, { role: 'assistant', content: response.data.choices[0].message.content });
-
-        fs.appendFileSync(outputFilePath, JSON.stringify({messages: history}, null) + '\n');
-    });
+  }
+  process.exit(0);
 }
 
 main();
